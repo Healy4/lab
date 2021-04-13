9152677c805610ad6171d58ffee7ace102246a59
Healy4 <31487326+Healy4@users.noreply.github.com>
2021-04-13 22:00:50 +0300

    Update Ur.h

--- Ur.h
+++ Ur.h
@@ -1,10 +1,10 @@
 #ifndef UR_H
 #define UR_H
 #include "My.h"
-
-class Ur : public My {
-public:
-	Ur(My*, string);
-	//~Ur();
+	
+class Ur : public My 
+{
+	public:
+		Ur(My* parent, string name = "Default");
 };
 #endif
